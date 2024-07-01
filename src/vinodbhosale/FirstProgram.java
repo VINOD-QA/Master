c02714fb61db8101daa90bd7d883381c7942f764
Vinod Bhosale <vinod_bhosale@persistent.com>
2024-07-01 16:22:13 +0530

    first commit

---
+++ src/vinodbhosale/FirstProgram.java
@@ -0,0 +1,12 @@
+package vinodbhosale;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi..Vinod Bhosale");
+
+	}
+
+}
